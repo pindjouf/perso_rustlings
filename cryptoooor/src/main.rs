823358cf8e134c5baf8ac4f50e5e0d7c83297cbc
love <esaubukasa@protonmail.com>
2024-09-17 01:37:01 +0200

    this is a mess

--- cryptoooor/src/main.rs
+++ cryptoooor/src/main.rs
@@ -1,36 +1,53 @@
 use aes_gcm::{
-    aead::{Aead, AeadCore, KeyInit, OsRng},
-    Aes256Gcm, Nonce, Key // Or `Aes128Gcm`
+    aead::{Aead, AeadCore, KeyInit},
+    Aes256Gcm, Nonce, Key
 };
+use rprompt;
 use std::fs;
+use std::io::{self, Write, Read};
+use scrypt::{
+    password_hash::{
+        rand_core::OsRng,
+        PasswordHash, PasswordHasher, PasswordVerifier, SaltString
+    },
+    Scrypt,
+    Params
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let key = Aes256Gcm::generate_key(OsRng);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-    let cipher = Aes256Gcm::new(&key);
 
-    fs::write("key", cipher);
-
-    let message = load_file("message.txt").expect("Couldn't load your file");
-
-    let ciphertext = cipher.encrypt(&nonce, message.as_ref()).expect("Can't encrypt your file");
-    let _ = fs::write("message.bin", ciphertext);
-
-    let cryptoFile = load_file("message.bin").expect("damn");
-    let plaintext = cipher.decrypt(&nonce, cryptoFile.as_ref()).expect("Can't decrypt your file");
-    let _ = fs::write("decrypted.txt", plaintext);
-
+    let _ = derive_key();
     Ok(())
 }
 
-fn load_file(file: &str) -> Result<Vec<u8>, std::io::Error> {
-    fs::read(file)
+fn encrypt(content: Vec<u8>, key: &Key) -> Result<(Vec<u8>, Nonce), Box<dyn std::error::Error>> {
+    let cipher = Aes256Gcm::new(key);
+
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let ciphertext = cipher.encrypt(&nonce, &*content.as_ref())?;
 }
 
-fn encrypt(arg: Type) -> RetType {
-    todo!();
-}
+//fn decrypt(ciphertext: Vec<u8>, nonce: Nonce, key: &Key) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+//
+//}
 
-fn decrypt(arg: Type) -> RetType {
-    todo!();
+//fn read_encrypted_file(file_path: &str) -> Result<(Nonce, Vec<u8>), io::Error> {
+//
+//}
+//
+fn derive_key() -> Result<(), Box<dyn std::error::Error>> {
+    // password prompt
+    let binding = rprompt::prompt_reply("Enter your password: ").unwrap();
+    
+    // convert to bytes
+    let password = binding.as_bytes();
+
+    // gen a salt
+    let salt = SaltString::generate(&mut OsRng);
+
+    // 
+    let password_hash = Scrypt.hash_password(password, &salt)?;
+
+    fs::write("hash.txt", password_hash.to_string())?;
+    Ok(())
 }
